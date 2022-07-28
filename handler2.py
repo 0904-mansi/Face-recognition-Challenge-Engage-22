cce257f192edbd664eefd2da1fc9e52a724c7ad4
Mansi Mishra <mansimishra510@gmail.com>
2022-07-28 16:22:40 +0530

    Update handler2.py

--- handler2.py
+++ handler2.py
@@ -1,22 +1,5 @@
 # import modules
-import csv
 import pymysql
-
-# function for insert data in Missing.csv file 
-def insertData(data):
-     # heading of csv file
-    field = ["Report-ID", "Name","Father's Name","Address", "Phone",
-    "Gender", "DOB(yyyy-mm-dd)", "Identification", "Date of Missing(yyyy-mm-dd)",
-    "Place of Missing"]
-     # data of missing person to be written in csv file
-    x = [data['Report-ID'], data['Name'], data["Father's Name"], data['Address'], data['Phone'], data['Gender'], data['DOB(yyyy-mm-dd)'],
-         data['Identification'], data['Date of Missing(yyyy-mm-dd)'], data["Place of Missing"] ]
-    filen = "Missing.csv"
-    with open(filen, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-          # writing the information into csv file
-        csvwriter.writerow(field)
-        csvwriter.writerow(x)
         
 # function for insert data in mysql database        
 def insertData(data):
