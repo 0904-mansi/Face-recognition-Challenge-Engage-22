3a7b91aece3d573e78538cb2c979d714161bf593
Mansi Mishra <mansimishra510@gmail.com>
2022-07-28 23:12:35 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -8,23 +8,30 @@ def detect(path):
 
 # while video is playing read faces
     while(True):
-        ret,img = cam.read()
+        ret,frame = cam.read()
+         # Resize the Frame to improve speed
+        frame = imutils.resize(frame, width=450)
         # converting face into grayScale 
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         # detecting gray image using detectscale function
-        faces = facedetect.detectMultiScale(gray,1.3,5)
-       # for crooping face we need to draw a rectangle using these coordinates
-        for(x,y,w,h) in faces:
-            sampleNum+=1# increasing samplenum
-            cv2.imwrite('dataset/'+str(sampleNum)+'.jpg',gray[y:y+h,x:x+w]) # storing file in dataset folder
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.waitKey(1)
-            # show rectangle on face
-        cv2.imshow('face',img)
-        cv2.waitKey(1)
-        # we are storing only 100 images
-        if(sampleNum>100):
-            break
+          faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.1, # This tells how much the object’s size is reduced in each image.
+                    minNeighbors=5, # This parameter tells how many neighbours each rectangle candidate should consider.
+                    minSize=(25, 25) # This signifies the minimum possible size of an object to be detected. An object smaller than minSize would be ignored.
+                )
 
-    cam.release()
-    cv2.destroyAllWindows()
+        
+       # Draw a rectangle around the Faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting Frame
+    cv2.imshow('Video', frame)
+# waitkey() function of Python OpenCV allows users to display a window for given milliseconds or until any key is pressed.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# When everything is done, release the Capture
+cam.release()
+cv2.destroyAllWindows()
